91cff2f4a37386dbcab7a1effd6f16e2d9913e47
GiannettaGerardo <gp.gerardo@protonmail.com>
2026-06-12 12:15:16 +0200

    Added Builder options and optional key's password.
    - Added struct Options as input for the Builder. With this new struct, you can decide the build order: first trusted certs, then key/cert pairs, or vice versa;
    - Added the possibility to add a new (optional) password in SetPrivateKeyEntry, apart from the main Builder password.

--- pkcs12_extension.go
+++ pkcs12_extension.go
@@ -10,43 +10,50 @@ import (
 )
 
 type Builder struct {
-	enc             *Encoder
-	certBags        []safeBag
-	prvKeyBags      []safeBag
-	trustedBags     []safeBag
-	encodedPassword []byte
-	aliases         map[string]struct{}
+	enc              *Encoder
+	certBags         []safeBag
+	prvKeyBags       []safeBag
+	trustedBags      []safeBag
+	encodedPassword  []byte
+	aliases          map[string]struct{}
+	orderFirstTrusts bool
 }
 
-type PrvKeyStoreEntry struct {
-	PrivateKey  any
-	Certificate *x509.Certificate
-	CaCerts     []*x509.Certificate
+type Options struct {
+	orderFirstTrusts bool
+	prvKeyEntryLen   uint8
+	trustCertLen     uint8
 }
 
 var (
 	trustCertPkcs12Attribute = getTrustCertPkcs12Attribute()
 )
 
-// NewBuilder creates a new PKCS12 builder with a standard password.
+// NewBuilder creates a new PKCS12 builder with a main builder password, used
+// across all the PKCS12 object, except when other passwords are provided.
 // The password is copied, so it can be destroyed without problems.
-// The '...Len' parameters are optional and can be used to preallocate memory.
-func NewBuilder(enc *Encoder, password []byte, prvKeyEntryLen uint8, trustCertLen uint8) (Builder, error) {
+//
+// The Options are optional. They can be used to preallocate some memory and to
+// decide the build order (first trusted, then key/cert pairs, or vice versa).
+func NewBuilder(enc *Encoder, password []byte, opt Options) (Builder, error) {
 
 	var err error
+	if enc == nil {
+		return Builder{}, errors.New("pkcs12: encoder cannot be nil")
+	}
+
 	var s = Builder{
 		enc:     enc,
-		aliases: make(map[string]struct{}, prvKeyEntryLen+trustCertLen),
+		aliases: make(map[string]struct{}, opt.prvKeyEntryLen+opt.trustCertLen),
 	}
 
-	if prvKeyEntryLen > 0 {
-		s.certBags = make([]safeBag, 0, prvKeyEntryLen*3)
-		s.prvKeyBags = make([]safeBag, 0, prvKeyEntryLen)
+	if opt.prvKeyEntryLen > 0 {
+		s.certBags = make([]safeBag, 0, opt.prvKeyEntryLen*3)
+		s.prvKeyBags = make([]safeBag, 0, opt.prvKeyEntryLen)
 	}
-	if trustCertLen > 0 {
-		s.trustedBags = make([]safeBag, 0, trustCertLen)
+	if opt.trustCertLen > 0 {
+		s.trustedBags = make([]safeBag, 0, opt.trustCertLen)
 	}
-
 	if enc.macAlgorithm == nil && enc.certAlgorithm == nil && enc.keyAlgorithm == nil && len(password) != 0 {
 		return Builder{}, errors.New("pkcs12: password must be empty")
 	}
@@ -55,6 +62,9 @@ func NewBuilder(enc *Encoder, password []byte, prvKeyEntryLen uint8, trustCertLe
 	if err != nil {
 		return Builder{}, err
 	}
+
+	s.orderFirstTrusts = opt.orderFirstTrusts
+
 	return s, nil
 }
 
@@ -77,13 +87,24 @@ func (s *Builder) Reset(password []byte) error {
 // SetPrivateKeyEntry set a certificate with its private key and an optional
 // chain of trust, with a custom friendly name (alias). Private Key and Certificate
 // share the same localKeyId and friendlyName.
+//
+// Password is optional: if not provided, the builder main password will be used.
+// If it's provided, the encoded version created at run time will be erased after the end
+// of this method.
+//
 // Does not check the consistency of the private key with the public key.
-func (s *Builder) SetPrivateKeyEntry(friendlyName string, entry PrvKeyStoreEntry) error {
+func (s *Builder) SetPrivateKeyEntry(
+	friendlyName string,
+	privateKey any,
+	certificate *x509.Certificate,
+	caCerts []*x509.Certificate,
+	password []byte,
+) error {
 
-	if entry.Certificate == nil {
+	if certificate == nil {
 		return errors.New("pkcs12: empty certificate")
 	}
-	if entry.PrivateKey == nil {
+	if privateKey == nil {
 		return errors.New("pkcs12: empty private key")
 	}
 	if friendlyName == "" {
@@ -95,7 +116,7 @@ func (s *Builder) SetPrivateKeyEntry(friendlyName string, entry PrvKeyStoreEntry
 		return err
 	}
 
-	var certFingerprint = sha1.Sum(entry.Certificate.Raw)
+	var certFingerprint = sha1.Sum(certificate.Raw)
 	var localKeyIdAttr pkcs12Attribute
 	localKeyIdAttr.Id = oidLocalKeyID
 	localKeyIdAttr.Value.Class = 0
@@ -105,13 +126,13 @@ func (s *Builder) SetPrivateKeyEntry(friendlyName string, entry PrvKeyStoreEntry
 		return err
 	}
 
-	if certBag, err := makeCertBag(entry.Certificate.Raw, []pkcs12Attribute{localKeyIdAttr, friendlyNameAttr}); err != nil {
+	if certBag, err := makeCertBag(certificate.Raw, []pkcs12Attribute{localKeyIdAttr, friendlyNameAttr}); err != nil {
 		return err
 	} else {
 		s.certBags = append(s.certBags, *certBag)
 	}
 
-	for _, cert := range entry.CaCerts {
+	for _, cert := range caCerts {
 		if certBag, err := makeCertBag(cert.Raw, []pkcs12Attribute{}); err != nil {
 			return err
 		} else {
@@ -126,15 +147,23 @@ func (s *Builder) SetPrivateKeyEntry(friendlyName string, entry PrvKeyStoreEntry
 		keyBag.Value.Class = 2
 		keyBag.Value.Tag = 0
 		keyBag.Value.IsCompound = true
-		if keyBag.Value.Bytes, err = x509.MarshalPKCS8PrivateKey(entry.PrivateKey); err != nil {
+		if keyBag.Value.Bytes, err = x509.MarshalPKCS8PrivateKey(privateKey); err != nil {
 			return err
 		}
 	} else {
+		var encodedPass = s.encodedPassword
+		if len(password) != 0 {
+			if encodedPass, err = bmpStringZeroTerminatedFromBytes(password); err != nil {
+				return err
+			}
+			defer zeroingBytes(encodedPass)
+		}
+
 		keyBag.Id = oidPKCS8ShroundedKeyBag
 		keyBag.Value.Class = 2
 		keyBag.Value.Tag = 0
 		keyBag.Value.IsCompound = true
-		if keyBag.Value.Bytes, err = encodePkcs8ShroudedKeyBag(enc.rand, entry.PrivateKey, enc.keyAlgorithm, s.encodedPassword, enc.encryptionIterations, enc.saltLen); err != nil {
+		if keyBag.Value.Bytes, err = encodePkcs8ShroudedKeyBag(enc.rand, privateKey, enc.keyAlgorithm, encodedPass, enc.encryptionIterations, enc.saltLen); err != nil {
 			return err
 		}
 	}
@@ -169,11 +198,7 @@ func (s *Builder) SetTrustedCertificateEntry(friendlyName string, certificate *x
 	return nil
 }
 
-// Build the entire PKCS12 object in memory structured like this:
-//  1. AuthenticatedSafe: Trusted certificates;
-//  2. AuthenticatedSafe: PrivateKeyEntry certificates;
-//  3. AuthenticatedSafe: PrivateKeyEntry private keys;
-//
+// Build the entire PKCS12 object in memory.
 // Erase the internal encoded copy of the password regardless of the result.
 func (s *Builder) Build() (pfxData []byte, err error) {
 	defer zeroingBytes(s.encodedPassword)
@@ -184,28 +209,24 @@ func (s *Builder) Build() (pfxData []byte, err error) {
 
 	var authenticatedSafe = make([]contentInfo, 0, 3)
 
-	// Add Trusted Certificates.
-	if len(s.trustedBags) != 0 {
-		ci, err := makeSafeContents(enc.rand, s.trustedBags, enc.certAlgorithm, s.encodedPassword, enc.encryptionIterations, enc.saltLen)
-		if err != nil {
+	if s.orderFirstTrusts {
+		// 1. add Trusted Certificates.
+		if authenticatedSafe, err = s.addTrustedCertificates(authenticatedSafe); err != nil {
 			return nil, err
 		}
-		authenticatedSafe = append(authenticatedSafe, ci)
-	}
-
-	// Add Certificates with their Private Keys and Chains.
-	if len(s.certBags) != 0 {
-		ci, err := makeSafeContents(enc.rand, s.certBags, enc.certAlgorithm, s.encodedPassword, enc.encryptionIterations, enc.saltLen)
-		if err != nil {
+		// 2. add Certificates with their Private Keys and Chains.
+		if authenticatedSafe, err = s.addKeyCertPair(authenticatedSafe); err != nil {
 			return nil, err
 		}
-		authenticatedSafe = append(authenticatedSafe, ci)
-
-		ci, err = makeSafeContents(enc.rand, s.prvKeyBags, nil, nil, 0, 0)
-		if err != nil {
+	} else {
+		// 1. add Certificates with their Private Keys and Chains.
+		if authenticatedSafe, err = s.addKeyCertPair(authenticatedSafe); err != nil {
 			return nil, err
 		}
-		authenticatedSafe = append(authenticatedSafe, ci)
+		// 2. add Trusted Certificates.
+		if authenticatedSafe, err = s.addTrustedCertificates(authenticatedSafe); err != nil {
+			return nil, err
+		}
 	}
 
 	var authenticatedSafeBytes []byte
@@ -246,6 +267,34 @@ func (s *Builder) Build() (pfxData []byte, err error) {
 		return nil, fmt.Errorf("pkcs12: error writing P12 data: %w", err)
 	}
 	return
+}
+
+func (s *Builder) addTrustedCertificates(authenticatedSafe []contentInfo) ([]contentInfo, error) {
+	if len(s.trustedBags) != 0 {
+		ci, err := makeSafeContents(s.enc.rand, s.trustedBags, s.enc.certAlgorithm, s.encodedPassword, s.enc.encryptionIterations, s.enc.saltLen)
+		if err != nil {
+			return nil, err
+		}
+		return append(authenticatedSafe, ci), nil
+	}
+	return authenticatedSafe, nil
+}
+
+func (s *Builder) addKeyCertPair(authenticatedSafe []contentInfo) ([]contentInfo, error) {
+	if len(s.certBags) != 0 {
+		ci, err := makeSafeContents(s.enc.rand, s.certBags, s.enc.certAlgorithm, s.encodedPassword, s.enc.encryptionIterations, s.enc.saltLen)
+		if err != nil {
+			return nil, err
+		}
+		authenticatedSafe = append(authenticatedSafe, ci)
+
+		ci, err = makeSafeContents(s.enc.rand, s.prvKeyBags, nil, nil, 0, 0)
+		if err != nil {
+			return nil, err
+		}
+		return append(authenticatedSafe, ci), nil
+	}
+	return authenticatedSafe, nil
 }
 
 func (s *Builder) encodeFriendlyName(friendlyName string) (pkcs12Attribute, error) {
